d15fb9ee511d4f098a55518ec827215f10b51a95
gacky35 <s17t258@stu.kagawa-u.ac.jp>
2019-09-04 23:41:25 +0900

    modified [WIP]

--- golfa.py
+++ golfa.py
@@ -1,10 +1,8 @@
 x,y = (int(a) for a in input().split())
-if x>=0 and y>0:
-    print("{}@{}".format("."*x, "."*y))
-if x>=0 and y==0:
-    print("{}".format("."*x))
-if x>=0 and y<0:
-    print("{}@@@{}".format("."*x, "."*(-y)))
+if x>=0:
+    print("{}".format("."*x), end='')
+    if y != 0:
+        print("@{}".format("."*y)) if y>0 else print("@@@{}".format("."*(-y)))
 if x<0 and y>=0:
     print("@{}@{}".format("."*y, "."*(-x)))
 if x<0 and y<0:
